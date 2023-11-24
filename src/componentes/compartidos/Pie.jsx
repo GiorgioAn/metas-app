ab93720e8c9c6105740c609eebb993616da10cc2
GiorgioAn <giorgioantoine@gmail.com>
2023-11-24 11:54:11 -0400

    Actualizacion archivos TailWind

---
+++ src/componentes/compartidos/Pie.jsx
@@ -0,0 +1,9 @@
+function Pie() {
+    return (
+      <footer>
+        <small>Desarrollado y diseñado por Jorge Antonio García Ortiz. ©2024. Todos los derechos reservados</small>
+      </footer>
+    );
+  }
+  
+  export default Pie;
